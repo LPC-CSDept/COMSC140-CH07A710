7a0142fbe27c86bc5b38e8a0f787cfe3177d9061
Kyuwoong Lee <kwooler@gmail.com>
2022-10-02 18:37:31 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ data2 = [90, 80, 70, 100]
 labels = ['Math', 'English', 'Physics', 'Computer']
 names = ['Bill', 'Mary']
 
+fig, ax = plt.subplots()
+
 # ******************************
 # Make your code
 # ******************************
+
+
+fig.savefig('A10.png')
