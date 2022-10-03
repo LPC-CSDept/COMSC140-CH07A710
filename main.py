01b72fe5c82791bfab6cf7d3fdb601372aae9848
Kyuwoong Lee <kwooler@gmail.com>
2022-10-02 18:36:32 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+data1 = [100, 90, 80, 60]
+data2 = [90, 80, 70, 100]
+labels = ['Math', 'English', 'Physics', 'Computer']
+names = ['Bill', 'Mary']
+
+# ******************************
+# Make your code
+# ******************************
